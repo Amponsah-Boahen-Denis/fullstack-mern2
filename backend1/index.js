56dc6f0b721d5d68f9400e6c7984cad691a9b4d0
Amponsah  Denis Boahen <92126671+Amponsah-Boahen-Denis@users.noreply.github.com>
2024-07-26 06:38:52 -0700

    Update index.js

--- backend1/index.js
+++ backend1/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(cors({
   origin:["https://fullstack-mern-front.vercel.app"],
-  methods: ["POST", "GET"],
+  methods: ["GET", "POST"],
   credentials: true
 }
 ));
