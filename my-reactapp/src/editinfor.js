502d1016e1176d10620f557c2f1a74fcec2c1677
Amponsah  Denis Boahen <92126671+Amponsah-Boahen-Denis@users.noreply.github.com>
2024-10-06 06:02:48 -0700

    Update editinfor.js

--- my-reactapp/src/editinfor.js
+++ my-reactapp/src/editinfor.js
@@ -1,3 +1,121 @@
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import { useParams, useNavigate } from 'react-router-dom';
+// import './css/edit.css';
+
+// function EditRecord() {
+//   const { id } = useParams();
+//   const navigate = useNavigate();
+//   const [accounts, setAccounts] = useState([]);
+//   const [selectedAccountId, setSelectedAccountId] = useState(id);
+//   const [formData, setFormData] = useState({
+//     Username: '',
+//     Password: '',
+//     Notes: '',
+//     URL: ''
+//   });
+//   axios.defaults.withCredentials = true;
+
+//   useEffect(() => {
+//     axios.get('https://fullstack-mern-api.vercel.app/account')
+//       .then(response => {
+//         setAccounts(response.data);
+//         const account = response.data.find(acc => acc._id === selectedAccountId);
+//         if (account) {
+//           setFormData({
+//             Username: account.Username,
+//             Password: account.Password,
+//             Notes: account.Notes,
+//             URL: account.URL
+//           });
+//         }
+//       })
+//       .catch(error => {
+//         console.error('There was an error fetching the data!', error);
+//       });
+//   }, [selectedAccountId]);
+
+//   const handleAccountChange = (event) => {
+//     setSelectedAccountId(event.target.value);
+//   };
+
+//   const handleChange = (event) => {
+//     const { name, value } = event.target;
+//     setFormData(prevState => ({
+//       ...prevState,
+//       [name]: value
+//     }));
+//   };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     console.log('Submitting form with data:', formData);
+//     axios.put(`https://fullstack-mern-api.vercel.app/account/${selectedAccountId}`, formData)
+//       .then(response => {
+//         console.log('Successfully updated account:', response.data);
+//         navigate('/'); // Navigate back to the home page
+//       })
+//       .catch(error => {
+//         console.error('There was an error updating the data!', error);
+//       });
+//   };
+
+//   return (
+//     <div className='container'>
+//       <h1 className='h2'>Edit Social Media Account</h1>
+//       <div>
+//         <select className='sel' value={selectedAccountId} onChange={handleAccountChange}>
+//           <option value="">Select an account</option>
+//           {accounts.map(account => (
+//             <option key={account._id} value={account._id}>{account.Description}</option>
+//           ))}
+//         </select>
+//       </div>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           <label className='label'>Username:</label>
+//           <input className='input'
+//             type="text"
+//             name="Username"
+//             value={formData.Username}
+//             onChange={handleChange}
+//           />
+//         </div>
+//         <div>
+//           <label className='label'>Password:</label>
+//           <input className='input'
+//             type="text"
+//             name="Password"
+//             value={formData.Password}
+//             onChange={handleChange}
+//           />
+//         </div>
+//         <div>
+//           <label className='label'>Notes:</label>
+//           <input className='input'
+//             type="text"
+//             name="Notes"
+//             value={formData.Notes}
+//             onChange={handleChange}
+//           />
+//         </div>
+//         <div>
+//           <label className='label'>URL:</label>
+//           <input className='input'
+//             type="text"
+//             name="URL"
+//             value={formData.URL}
+//             onChange={handleChange}
+//           />
+//         </div>
+//         <button type="submit" className='butt'> Update & Exit</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default EditRecord;
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
@@ -14,10 +132,9 @@ function EditRecord() {
     Notes: '',
     URL: ''
   });
-  axios.defaults.withCredentials = true;
 
   useEffect(() => {
-    axios.get('https://fullstack-mern-api.vercel.app/account')
+    axios.get('http://localhost:5000/account')
       .then(response => {
         setAccounts(response.data);
         const account = response.data.find(acc => acc._id === selectedAccountId);
@@ -49,23 +166,28 @@ function EditRecord() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('Submitting form with data:', formData);
     axios.put(`https://fullstack-mern-api.vercel.app/account/${selectedAccountId}`, formData)
       .then(response => {
-        console.log('Successfully updated account:', response.data);
-        navigate('/'); // Navigate back to the home page
+        //navigate('/'); // Navigate back to the home page
       })
       .catch(error => {
         console.error('There was an error updating the data!', error);
       });
   };
 
+  const handleSubmit2 = (event) => {
+    event.preventDefault();
+        navigate('/password'); // Navigate back to the home page
+  };
+
   return (
     <div className='container'>
-      <h1 className='h2'>Edit Social Media Account</h1>
+      <h1 className='h2'>PASSWORD MANAGER</h1>
+      <p>This app helps you securely manage and track your social media account credentials.</p>
+      <button onClick={handleSubmit2} className='butt'> Add Account</button>
       <div>
         <select className='sel' value={selectedAccountId} onChange={handleAccountChange}>
-          <option value="">Select an account</option>
+          <option value="">Select already existed account</option>
           {accounts.map(account => (
             <option key={account._id} value={account._id}>{account.Description}</option>
           ))}
@@ -90,15 +212,7 @@ function EditRecord() {
             onChange={handleChange}
           />
         </div>
-        <div>
-          <label className='label'>Notes:</label>
-          <input className='input'
-            type="text"
-            name="Notes"
-            value={formData.Notes}
-            onChange={handleChange}
-          />
-        </div>
+
         <div>
           <label className='label'>URL:</label>
           <input className='input'
@@ -108,10 +222,23 @@ function EditRecord() {
             onChange={handleChange}
           />
         </div>
-        <button type="submit" className='butt'> Update & Exit</button>
+
+        <div>
+          <label className='label'>Notes:</label>
+          <input className='input'
+            type="text"
+            name="Notes"
+            value={formData.Notes}
+            onChange={handleChange}
+          />
+        </div>
+       
+        <button type="submit" className='butt'>Edit Above & Update</button>
+       
       </form>
     </div>
   );
 }
 
 export default EditRecord;
+
